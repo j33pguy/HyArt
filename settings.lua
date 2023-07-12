66654ee272354c68e886b517a2ec1a8d3b2613c4
j33pguy <jeepguygaming@gmail.com>
2023-07-11 21:45:29 -0400

    starting to look at moving over to per-user setting....god i hate languages that use {}....

--- settings.lua
+++ settings.lua
@@ -1,15 +1,19 @@
--- After thinking about it, I had mod working and started looking at 
--- moving it from startup to a 'slider value'
+-- Peronsal Notes:
+-- Settings.lua is used for mod menu in settigns
+-- you can specify type, name, setting type ( runtime/per-player), default value, and order <ABCDEF>
 
--- Lets get this back to a working INIT state
--- ESP Now that I have git
+-- for this mod, I am thinking 2 settings need to happen: 
+-- enable/disable ( if disable, go to game defautl ?)
+-- time ( this will be an int )
+--      Might have to set a max value if causes crash b/c item has been on ground too long
 
-data:extend
-{
+data:extend {
     {
         type = "int-setting",
         name = "artillery-shell-particle-life-time",
-        setting_type = "runtime-global",
+        -- setting_type = "runtime-global",
+        setting_type = "runtime-per-user", -- trying to get this to run as a per-user setting
+
 
          -- Potential slider or user input????
         -- default_value = 20,
