2db7958fe01bc483828e801fdf9d89450b765f0f
j33pguy <jeepguygaming@gmail.com>
2023-07-11 22:19:25 -0400

    cleaning up files, removing my notes, working on Settings.lua

---
+++ TODO.md
@@ -0,0 +1,15 @@
+# v 0.1.5
+
+Starting to move the mod to a per-user setting
+
+### Next steps
+
+//Settings.lua
+TODO:
+* bool for turning mod on/off
+    * if off, default to game setting
+* int for time on ground, currently in ms ( need to verify )
+    * to keep it simple have user put in as seconds and convert via func
+
+### Next Next steps
+* will need to feed both of those settings into funtions
--- settings.lua
+++ settings.lua
@@ -1,22 +1,15 @@
--- Peronsal Notes:
--- Settings.lua is used for mod menu in settigns
--- you can specify type, name, setting type ( runtime/per-player), default value, and order <ABCDEF>
-
--- for this mod, I am thinking 2 settings need to happen: 
--- enable/disable ( if disable, go to game defautl ?)
--- time ( this will be an int )
---      Might have to set a max value if causes crash b/c item has been on ground too long
-
 data:extend {
+    {
+        type = "bool-setting",
+        name = "artillery-shell-particle-enable",
+        setting_type = "runtime-per-user", 
+        order = "a",
+
+    },
     {
         type = "int-setting",
         name = "artillery-shell-particle-life-time",
-        setting_type = "runtime-global",
-        -- setting_type = "runtime-per-user", -- trying to get this to run as a per-user setting
-
-
-         -- Potential slider or user input????
-        -- default_value = 20,
-        -- minimum_value = 5,
+        setting_type = "runtime-per-user", 
+        order =  "b",
     }
 }
